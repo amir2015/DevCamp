a29a6a29e8abaaeee58941ea050bd73127c64c97
amir <zahranovich2@gmail.com>
2017-11-05 21:30:53 -0800

    integrated friendly route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
-
+  #want to override original resource givings, add except  
+  resources :portfolios , except: [:show] 
+  get 'portfolio/id' , to: 'portfolios#show', as: 'portfolio_show'
+  
   get 'about' , to: 'pages#about'
   
   get 'contact', to: 'pages#contact'
